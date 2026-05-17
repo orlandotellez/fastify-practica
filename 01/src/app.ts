72cb2693c5881d8e8cad5f37e88a89c9067901a9
orlandotellez <espadachinkinno@gmail.com>
2026-05-16 23:56:51 -0600

    feat: agregar rate limit para limitar request por ip en tiempo determinado

--- 01/src/app.ts
+++ 01/src/app.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify"
 import helmet from "@fastify/helmet"
 import cors from "@fastify/cors"
 import compress from "@fastify/compress"
+import rateLimit from "@fastify/rate-limit"
 
 export const buildApp = async () => {
   const app = Fastify({
@@ -22,6 +23,12 @@ export const buildApp = async () => {
 
   // Comprime respuestas con gzip.
   await app.register(compress)
+
+  // Limita a 100 requests/min por IP.
+  await app.register(rateLimit, {
+    max: 100,
+    timeWindow: "1 minute"
+  })
 
   return app
 }
